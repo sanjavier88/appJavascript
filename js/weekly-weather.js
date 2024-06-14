588cc4f24b56989d759d9be5ea9b3b170df39dd2
jorge monsalve <planetdental2020@gmail.com>
2024-06-14 16:41:04 -0500

    ultimos toques

--- js/weekly-weather.js
+++ js/weekly-weather.js
@@ -2,15 +2,16 @@ import {getWeeklytWeather} from './services/weather.js'
 import {getLatLon} from './geolocation.js'
 import {formatWeeklist} from './utils/format-data.js'
 import {createDom} from './utils/dom.js'
-import { createPeriodTime} from './period-time.js'  
+import {createPeriodTime} from './period-time.js'  
+import draggable  from './draggble.js'
 
 
 function tabPanelTemplate(id) {
     return `
     <div class="tabPanel" tabindex="0" aria-labelledby="tab-${id}">
     <div class="dayWeather" id="dayWeather-${id}">
-      <ul style="color:white" class="dayWeather-list" id="dayWeather-list-${id}">
-            Tab Panel${id}
+      <ul  class="dayWeather-list" id="dayWeather-list-${id}">
+          
         
       </ul>
     </div>
@@ -20,30 +21,32 @@ function tabPanelTemplate(id) {
 
 function createTabPanel(id) {
     const $panel = createDom(tabPanelTemplate(id))
-    if($panel > 0){
+    if(id > 0){
         $panel.hidden = true
     }
     return $panel
 }
 
 function  configWeeklyWeather( weeklist) {
-    const $container = document.querySelector('.weeklyWeather')
+    const $container = document.querySelector('.tabs')
     weeklist.forEach((day, index)=> {
         const $panel = createTabPanel(index)
         $container.append($panel)
         day.forEach((weather, indexWeather)=>{
-            $panel.append( createPeriodTime(weather))
+            $panel.querySelector('.dayWeather-list').append( createPeriodTime(weather))
         })
     })
-
+    
 }
 
 
 export default async function weeklyWeather(){
+    const $container = document.querySelector('.weeklyWeather')
     const {lat,lon, isError} = await getLatLon()
     if(isError) return console.log('Ah ocurrido un error ubicandote')
     const {isError: weeklyWeatherError, data: weather} = await getWeeklytWeather(lat, lon)
     if(weeklyWeatherError) return console.log('oh a ocurrido un error trayendo el pronostico  del clima')
     const weeklist = formatWeeklist(weather.list)
     configWeeklyWeather( weeklist)
+    draggable($container)
 }
